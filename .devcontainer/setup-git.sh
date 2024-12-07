4458d8693d3da4b69214bf642fdef1d829e45454
jboilard1994 <jboilard1994@gmail.com>
2024-12-07 19:03:33 +0000

    Proper installation and configuration of git in devcontainer

--- .devcontainer/setup-git.sh
+++ .devcontainer/setup-git.sh
@@ -1,8 +1,5 @@
 #!/bin/sh
-apt-get update -qqy
-apt-get install -y git
-
-git config --global user.name "${GIT_USERNAME}"
-git config --global user.email "${GIT_EMAIL}"
+git config --global user.name $GIT_USERNAME
+git config --global user.email $GIT_EMAIL
 git config --global credential.useHttpPath true
 git config --global core.filemode false
