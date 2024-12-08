9893f59b21e73bbb3a3568b171d79aeafad0b5bb
jboilard1994 <jboilard1994@gmail.com>
2024-12-08 19:15:46 +0000

    disable autocrlf when loading project in devcontainer in devcontainer oncreate shel script + set to LF for .sh files to avoid execution of the setup shell script on container creation + revert old attributes eol fixing

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -5,3 +5,4 @@ git config --global user.name $GIT_USERNAME
 git config --global user.email $GIT_EMAIL
 git config --global credential.useHttpPath true
 git config --global core.filemode false
+git config --global core.autocrlf false
